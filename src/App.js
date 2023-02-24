b8ca993dcf67e36289aba3076ac38c9ff30fcd53
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-24 15:40:46 +0300

    Update Routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
-const App = () => <h1>Abdii</h1>;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from 'components/Nav';
+import Books from 'Routes/Books';
+import Categories from 'Routes/Categories';
+
+const App = () => (
+  <BrowserRouter>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+
+  </BrowserRouter>
+);
 
 export default App;
