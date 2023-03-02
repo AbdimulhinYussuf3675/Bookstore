bc2bdd95303fb84cf17771531b3d2d7806a85301
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-03 00:07:16 +0300

    Fetch API

---
+++ src/fetchAPI/BookApiStore.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+
+const APP_ID = 'RjL3QgeYBlpVRvUzAjxl';
+
+const http = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const getAll = () => http.get(`/apps/${APP_ID}/books`);
+
+const create = ({
+  id, author, title, category,
+}) => http.post(`/apps/${APP_ID}/books`, {
+  item_id: id,
+  author,
+  title,
+  category,
+});
+
+const remove = (id) => http.delete(`/apps/${APP_ID}/books/${id}`);
+
+const BookStoreService = {
+  getAll,
+  create,
+  remove,
+};
+
+export default BookStoreService;
