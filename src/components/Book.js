475da0cdfc1a6dd68e1e1d090e9abd0ffb8c8ceb
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-03 00:08:34 +0300

    Update the page and props

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,12 +4,13 @@ import Buttons from './Buttons';
 
 const Book = ({ book }) => {
   const {
-    id, title, author,
+    id, title, author, progress, category,
   } = book;
 
   return (
     <div className="book">
       <div className="book-info">
+        <span className="book-category">{category}</span>
         <h2 className="book-title">{title}</h2>
         <h4 className="book-author">{author}</h4>
         <Buttons id={id} />
@@ -17,13 +18,14 @@ const Book = ({ book }) => {
       <div className="progress-box">
         <div className="progress-bar" />
         <div className="percent-display">
+          <span className="percent">{progress.toString().concat('%')}</span>
           <span>Completed</span>
         </div>
       </div>
       <div className="vertical-divider" />
       <div className="status-container">
         <h2>Current Chapter</h2>
-        <h3 className="chapter">Chapter 13</h3>
+        <h3 className="chapter">Chapter 19</h3>
         <button type="button" className="update-btn">
           Update Progress
         </button>
@@ -37,6 +39,8 @@ Book.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
+    progress: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
 };
 
