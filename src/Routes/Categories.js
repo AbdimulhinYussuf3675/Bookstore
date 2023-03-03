c34cdd47ce12ca4832b96fe562dc15407570bef4
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-03 09:01:52 +0300

    Updated className

--- src/Routes/Categories.js
+++ src/Routes/Categories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { checkStatus } from 'redux/categories/categoriesSlice';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+import styles from '../styles/Categories.module.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,18 @@ const Categories = () => {
   };
 
   return (
-    <article>
-      <h2>Categories</h2>
-      <h3>{categoriesStatus.length ? categoriesStatus : null}</h3>
-      <button type="button" onClick={handleClick}>
+    <article className={styles.categoriesPage}>
+      <h2>Categories Page</h2>
+      <h3>
+        {categoriesStatus === 'Kindly the following page is Under development' ? (
+          <div className={styles.statusPopup}>
+            <p>
+              {categoriesStatus}
+            </p>
+          </div>
+        ) : null}
+      </h3>
+      <button type="button" onClick={handleClick} className={styles.checkBtn}>
         Check Status
       </button>
     </article>
